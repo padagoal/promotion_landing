847fce8adc3d4cc00f99f27cdc653142193afd4f
Luis Guanes <luis@toky.co>
2020-07-03 20:14:01 -0400

    add delete all btn to admin

--- JValenApp/admin.py
+++ JValenApp/admin.py
@@ -17,7 +17,28 @@ class CustomPersonaVotanteAdmin(ImportExportModelAdmin):
     resource_class = PersonaVotanteAdminResource
     list_display = (('cedula'),('matricula'),('apellido'),('nombre'),('estaHabilitado'))
     search_fields = (('cedula'),('matricula'),('apellido'),('nombre'),('estaHabilitado'))
-    
+    actions = ['remove_all', ]
+
+    def remove_all(self, request, queryset):
+        queryset = Personas_votante.objects.all()
+        queryset.delete()
+    remove_all.short_description = "Delete all %(verbose_name_plural)s WARNING"
+    remove_all.acts_on_all = True
+
+    def changelist_view(self, request, extra_context=None):
+        try:
+            action = self.get_actions(request)[request.POST['action']][0]
+            action_acts_on_all = action.acts_on_all
+        except (KeyError, AttributeError):
+            action_acts_on_all = False
+
+        if action_acts_on_all:
+            post = request.POST.copy()
+            post.setlist(admin.helpers.ACTION_CHECKBOX_NAME,
+                        self.model.objects.values_list('cedula', flat=True))
+            request.POST = post
+
+        return admin.ModelAdmin.changelist_view(self, request, extra_context)
 
 
 admin.site.register(Personas_votante,CustomPersonaVotanteAdmin)
